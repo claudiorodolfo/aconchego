2933145b5dde3a8f6cd89edbf84ec308bbfe72c9
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2023-05-25 23:52:14 -0300

    Avaliacao

---
+++ visao/avaliacao/mostraravaliacao.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
+    <!-- JQuery JS -->  
+    <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>             
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!--Programador-->
+    <title>Opções de Gestão</title>          
+  </head> 
+  <body>
+    <br><br>
+    <div class="alert alert-success" role="alert">
+        APROVADO
+      </div>
+      <br>
+      <div class="alert alert-danger" role="alert">
+        REPROVADO
+      </div>
+      <br>
+    <table class="table table-bordered align-middle">
+        <tr>
+            <td colspan="8">FEEDBACK</td>
+        </tr>
+        <tr>
+            <td colspan="5" rowspan="2">ESTAKAZERO</td>
+            <td colspan="3">DATA</td>         
+        </tr> 
+        <tr>
+            <td colspan="3">  /  /  </td>
+        </tr>
+        <tr>
+            <td colspan="8"></td>
+        </tr>
+        <tr>
+            <td colspan="2">Nome</td>
+            <td colspan="6"></td>
+        </tr>
+        <tr>
+            <td colspan="2">Examinador</td>
+            <td colspan="6"></td>
+        </tr>       
+        <tr>
+            <td colspan="8"></td>
+        </tr>
+        <tr>
+            <td colspan="4" class="table-primary">CONDUTOR(A)</td>
+            <td colspan="4">CONDUZIDA(O)</td>
+        </tr> 
+        <tr>
+            <td colspan="8">MARCADA</td>
+        </tr> 
+        <tr>
+            <td colspan="3">Condução/Resposta</td>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td class="table-primary">4</td>
+            <td>5</td>
+        </tr> 
+        <tr>
+            <td colspan="3">Abraço</td>
+            <td>1</td>
+            <td>2</td>
+            <td class="table-primary">3</td>
+            <td>4</td>
+            <td>5</td>
+        </tr> 
+        <tr>
+            <td colspan="3">Mecânica</td>
+            <td>1</td>
+            <td>2</td>
+            <td class="table-primary">3</td>
+            <td>4</td>
+            <td>5</td>
+        </tr> 
+        <tr>
+            <td colspan="3">Ritmo</td>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td class="table-primary">5</td>
+        </tr> 
+        <tr>
+            <td colspan="3">Marcação</td>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td class="table-primary">4</td>
+            <td>5</td>
+        </tr> 
+    </table>
+  </body>
+</html>
