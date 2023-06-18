e664594d75daafa59c687e07a9f4a53e7c002518
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2023-06-17 21:40:39 -0300

    Update teste.java

--- visao/usuario/teste.java
+++ visao/usuario/teste.java
@@ -4,9 +4,30 @@ public class PilhaDinamica extends Empilhavel {
 		Object dadoTopo = null;
 		if (!estaVazia()) {
 			dadoTopo = ponteiroTopo.getDado();
+            //retrocede o ponteiro do Topo
+            ponteiroTopo = ponteiroTopo.getAnterior();
+            //elimina a referência ao elemento desempilhado
+            if (ponteiroTopo != null)
+                ponteiroTopo.setProximo(null);
+            //decrementa a quantidade de dados da estrutura
+            quantidade--;
 		} else {
 			System.out.println("Stack is empty!");
 		}
 		return dadoTopo;
+	}
+
+    void enfileirar(Object dado){
+		if(!estaCheia()) {
+			NodoDuplo novoNodo = new NodoDuplo();
+			novoNodo.setDado(dado);
+			if (ponteiroFim != null) {
+				ponteiroFim.setProximo(novoNodo);
+			}
+			novoNodo.setAnterior(ponteiroFim);
+			quantidade++;
+		} else {
+			System.out.println("Queue is full");
+		}
 	}
 }
