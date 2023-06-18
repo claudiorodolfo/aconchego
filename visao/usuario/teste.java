901fbc7267d6742c0a9c0b3294a76e327871d836
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2023-06-17 21:31:02 -0300

    Update teste.java

--- visao/usuario/teste.java
+++ visao/usuario/teste.java
@@ -4,8 +4,6 @@ public class PilhaDinamica extends Empilhavel {
 		Object dadoTopo = null;
 		if (!estaVazia()) {
 			dadoTopo = ponteiroTopo.getDado();
-            ponteiroTopo = ponteiroTopo.getAnterior();
-            quantidade--;
 		} else {
 			System.out.println("Stack is empty!");
 		}
