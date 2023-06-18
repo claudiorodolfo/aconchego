42cf9a6e3f1d1d35e17d138bfa090f19749484e3
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2023-06-17 21:59:37 -0300

    Update teste.java

--- visao/usuario/teste.java
+++ visao/usuario/teste.java
@@ -17,17 +17,39 @@ public class PilhaDinamica extends Empilhavel {
 		return dadoTopo;
 	}
 
-    void enfileirar(Object dado){
+    public void enfileirar(Object dado){
 		if(!estaCheia()) {
 			NodoDuplo novoNodo = new NodoDuplo();
 			novoNodo.setDado(dado);
 			if (ponteiroFim != null) {
 				ponteiroFim.setProximo(novoNodo);
 			}
+            //não deixar ponteiroInicio largado para trás
+            else {
+                ponteiroInicio = novoNodo;
+            }
 			novoNodo.setAnterior(ponteiroFim);
+            //ponteiroFim é jogado pra frente 
+            ponteiroFim = novoNodo;
 		    quantidade++;
 		} else {
 			System.out.println("Queue is full");
 		}
 	}
+
+    public void atualizar(int posicao, Object elemento) {
+        Object dado = null;
+		if (!estaVazia()) {
+			if ((posicao > 0) && (posicao <= quantidade)) {
+				NoDuplo ponteiroAuxiliar = ponteiroInicio;
+				for (int i = 0; i < posicao; i++) {
+					ponteiroAuxiliar = ponteiroAuxiliar.getProximo();
+				}
+				dado = ponteiroAuxiliar.getDado();
+            } else {
+               throw new UnderflowException();
+            }
+		} 
+	}
+    
 }
